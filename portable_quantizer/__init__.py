b08a7d6e1e0d883561ec9a101f2d474f84a621b4
zhen-dong <zhendong@berkeley.edu>
2021-03-16 03:50:09 -0700

    modify portable quantizer

--- portable_quantizer/__init__.py
+++ portable_quantizer/__init__.py
@@ -1,4 +1,2 @@
-from .quantization_utils.quantize_model import quantize_shufflenetv2_dcn, quantize_model, mix_quantize_model, list_model_parameters
-from .quantization_utils.data_utils import getData, getGaussianData
-from .train_utils import train, test
+from .quantization_utils.quantize_model import quantize_shufflenetv2_dcn
 from .quantization_utils.quant_utils import SymmetricQuantFunction, AsymmetricQuantFunction
